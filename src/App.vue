cd8b1519df456a1e7c3eb3cd319cbf3b619b7ca9
josé Zúñiga <jzunigar86@gmail.com>
2025-05-07 10:17:21 -0500

    se agrego un proyecto

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+  
+    <productoList />
+  </div>
+</template>
+
+<script>
+
+import productoList from './components/productoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+   
+    productoList
+  }
+};
+
+
+</script>
+
+
+
+<style>
+
+body {
+  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
